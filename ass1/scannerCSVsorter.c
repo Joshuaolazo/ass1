f84c77ebcc589930de418b4e3922729fb648bc99
Joshuaolazo <joshuaolazo@gmail.com>
2018-10-27 21:04:16 -0400

    merge both files and rename to lowercase

--- ass1/scannerCSVsorter.c
+++ ass1/scannerCSVsorter.c
@@ -6,10 +6,170 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
 #include <sys/mman.h>
-#include "scannerCSVsorter.h"
+#include "ScannerCSVsorter.h"
 
 
+int directory_crawler(char*);
+int dummy(void);
+
+int PRINT = 1;
+int PROCESSES = 1;
+
+int main(int argc, char *argv[]){
+	// Check for good arguments example below
+	// ./sorter -c movie_title -d thisdir -o thatdir
+	
+	// More descriptive error messages for bad flags
+	char* sorting_column = NULL;
+	char* sorting_directory = NULL;
+	sorting_directory = "./";
+	char* output_directory = NULL;
+	output_directory = "./";
+	switch (argc) {
+			// "Bad" Input Args
+			// Returns descriptive error message
+		case 0 :
+		case 1 :
+			fprintf(stderr,"%s\n","Not enough input arguments");
+			return -1;
+		case 2 :
+			if(strcmp(argv[1],"-c") == 0)
+				fprintf(stderr,"%s\n","Missing Sorting Column");
+			else
+				fprintf(stderr,"%s\n","Bad input arguments");
+			return -1;
+		case 4 :
+			if(strcmp(argv[3],"-d") == 0)
+				fprintf(stderr,"%s\n","Missing Sorting Directory");
+			else
+				fprintf(stderr,"%s\n","Bad input arguments");
+			return -1;
+		case 6 :
+			if(strcmp(argv[5],"-o") == 0)
+				fprintf(stderr,"%s\n","Missing Output Directory");
+			else
+				fprintf(stderr,"%s\n","Bad input arguments");
+			return -1;
+			
+			// "Good" input args
+			// Runs the code below each case
+			// each case below is a subset
+		case 7 :
+			if(strcmp(argv[5],"-o") != 0){
+				fprintf(stderr,"%s\n","Bad '-o' flag");
+				return -1;
+			}
+			output_directory= argv[6];
+		case 5:
+			if(strcmp(argv[3],"-d") != 0){
+				fprintf(stderr,"%s\n","Bad '-d' flag");
+				return -1;
+			}
+			sorting_directory= argv[4];
+		case 3 :
+			if(strcmp(argv[1],"-c") != 0){
+				fprintf(stderr,"%s\n","Bad '-c' flag");
+				return -1;
+			}
+			sorting_column = argv[2];
+			break;
+			// check if sorting column is in list.
+			
+			// Bad input if over 7 args
+		default:
+			fprintf(stderr,"%s\n","Too many input arguments");
+			return -1;
+	}
+	// Get origninal PID
+	int parent_pid= getpid();
+	
+	// Print Statements for output
+	printf("Initial PID: %d\n",parent_pid);
+	printf("PIDS of all child processes: ");
+	// Start sorting process
+	int  x = directory_crawler(sorting_directory);
+	printf("\nTotal number of processes: %d\n", PROCESSES);
+	return x;
+}
+
+
+int directory_crawler(char * sorting_directory){
+	DIR *directory;
+	struct dirent *dirent;
+	if( strlen(sorting_directory) == 0){
+		sorting_directory =".";
+	}
+	
+	directory = opendir(sorting_directory);
+	
+	if( !directory){
+		fprintf(stderr,"Cannot open directory: %s\n", strerror (errno));
+		return -1;
+	}
+	while(dirent){
+		const char * d_name;
+		dirent = readdir (directory);
+		if (! dirent) {
+			/* There are no more entries in this directory, so break
+			 out of the while loop. */
+			break;
+		}
+		d_name = dirent->d_name;
+		/* Print the name of the file and directory. */
+		if(PRINT==0){
+			printf ("%s\n",  d_name);
+		}
+		if (dirent->d_type & DT_DIR) {
+			// Check that the directory is not "d" or d's parent.
+			if (strcmp (d_name, "..") != 0 && strcmp (d_name, ".") != 0) {
+				// Recursively call "list_dir" with the new path.
+				int directorylen= (int) strlen(sorting_directory);
+				int d_namelen= (int) strlen(d_name);
+				char* new_directory = malloc(directorylen+d_namelen+1);
+				strcpy(new_directory, sorting_directory);
+				strcat(new_directory, d_name);
+				strcat(new_directory, "/");
+				if(PRINT==0){
+					printf("New directory: %s\n", new_directory);
+				}
+				int child = fork();
+				fflush(stdout);
+				int pid = getpid();
+				if(child ==0){
+					PROCESSES++;
+					if( PRINT>0)
+						printf("%d,",pid);
+					exit(1);
+				}
+				directory_crawler(new_directory);
+				
+			}
+			
+		}else{
+			int child = fork();
+			fflush(stdout);
+			int x = dummy();
+			x++;
+			int pid = getpid();
+			if(child ==0){
+				PROCESSES++;
+				if( PRINT>0)
+					printf("%d,",pid);
+				exit(1);
+			}
+			
+		}
+	}
+	return 0;
+}
+
+int dummy(){
+	//printf("Not a directory\n");
+	return 0;
+}
 //Where argv is what we're sorting by , file, output directory
 int sortCSV(char *argv, char* ffile, char* ddir){
 	
@@ -366,20 +526,6 @@ return 0;
 
 */
 
-int main(int argc, char **argv){
-	
-	//index to sort by
-	
-	char* a = malloc(sizeof(char)*20);
-	char* b = malloc(sizeof(char)*20);
-	char* c = malloc(sizeof(char)*30);
-	a="s";
-	b="test.csv";
-	c="franciscolivesinhismother'sbasement/pie/";
-	sortCSV(a,b,c);
-	
-	return 0;
-}
 
 
 
