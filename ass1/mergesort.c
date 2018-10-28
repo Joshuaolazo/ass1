baa8b3ffdb6c34a0519ec8b941bc63b6d6b5395b
Joshuaolazo <joshuaolazo@gmail.com>
2018-10-28 02:39:13 -0400

    Swap to my mergesort

--- ass1/mergesort.c
+++ ass1/mergesort.c
@@ -4,101 +4,69 @@
 #include <ctype.h>
 #include "ScannerCSVsorter.h"
 
-void mergesorter(Node** front) {
-	Node* one;
-	Node* two;
-	Node* head = *front;
-	
-	if(head->next == NULL||head == NULL)
+
+void merge_sort(Node** head)
+{
+	Node* headPtr = *head;
+	//if list is empty
+	if(headPtr == NULL)
+		return;
+	//if list of size 1
+	if(headPtr->next == NULL)
 		return;
 	
-	half(head, &one, &two);
-	
-	
-	mergesorter(&one);
-	mergesorter(&two);
-	
-	*front = merge(one,two);
-}
-
-Node* merge(Node* one,  Node* two) {
-	
-	Node* esp = NULL;
-	
-	if (one == NULL)
-		return two;
-	else if (two==NULL)
-		return one;
-	
-	//fix this later
-	int numcheck= isNumeric(one->data);
-	printf("NUMCHECK is: %d", numcheck);
-	if(numcheck == 1){
-		double aa, bb;
-		sscanf(one->cat, "%lf", &aa);
-		sscanf(two->cat, "%lf", &bb);
-		if (aa<=bb) {
-			esp = one;
-			esp->next = merge(one->next, two);
-		}
-		else{
-			esp = two;
-			esp->next = merge(one, two->next);
+	Node* end = headPtr;
+	Node* temp = headPtr->next;
+	while(temp != NULL){
+		temp = temp->next;
+		if(temp != NULL){
+			end = end->next;
+			temp = temp->next;
 		}
 	}
-	else{
-		if (strcmp(one->cat,two->cat)<=0) {
-			esp = one;
-			esp->next = merge(one->next, two);
-		}
-		else{
-			esp = two;
-			esp->next = merge(one, two->next);
-		}
-	}
-	return esp;
+	Node* list1 = headPtr;
+	Node* list2 = end->next;
+	end->next = NULL;
+	
+	merge_sort(&list1);
+	merge_sort(&list2);
+	*head = sort(list1, list2);
 }
 
-
-void half( Node* base, Node** first,  Node** last)
+Node* sort(Node* list1, Node* list2)
 {
-	Node* fast;
-	Node* slow;
-	slow = base;
-	fast = base->next;
+	Node* sorted = NULL;
+	if (list1 == NULL)
+		return(list2);
+	else if (list2==NULL)
+		return(list1);
 	
-	while (fast != NULL)
-	{
-		fast = fast->next;
-		if (fast != NULL) {
-			slow = slow->next;
-			fast = fast->next;
+	if(list1->movie->type == 1){ //Handles case of input being numeric
+		if ((list1->movie->colValue - list2->movie->colValue) < 0.0)
+		{
+			sorted = list1;
+			sorted->next = sort(list1->next, list2);
+		}
+		else
+		{
+			sorted = list2;
+			sorted->next = sort(list1, list2->next);
+		}
+	}
+	else if(list1->movie->type == 0){ //handles case of input being string
+		int s = strcmp(list1->movie->colData, list2->movie->colData);
+		
+		if (s < 0)
+		{
+			sorted = list1;
+			sorted->next = sort(list1->next, list2);
+		}
+		else
+		{
+			sorted = list2;
+			sorted->next = sort(list1, list2->next);
 		}
 	}
 	
-	
-	*first = base;
-	*last = slow->next;
-	slow->next = NULL;
-}
-
-//checks if string is numeric
-//returns 1 if numeric, 0 if non-numeric
-int isNumeric(char* data){
-	int i = 0;
-	int isNum = 1; //set to 0 if not numeric
-	int period = 0; //shouldnt be more than one period
-	int length = (int)(strlen(data));
-	for(i=0; i<length;i++){
-		char c = data[i];
-		if(c < '0' || c > '9'){
-			if(c == '.')
-				period++;
-			else
-				isNum = 0;
-			if(period > 1)
-				isNum = 0;
-		}
-	}
-	return isNum;
+	return(sorted);
 }
